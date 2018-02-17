c6d3b3bdf73a7f81a061a8fc995b061ef25ea997
Carlos Beltran <imthatcarlos@gmail.com>
2018-02-17 15:38:13 -0600

    remove /transactions

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -7,7 +7,6 @@ const appRoutes = [
     { path: "/client", name: "UVT Client", icon: "pe-7s-global", component: Client },
     { path: "/gateway", name: "UVT Gateway", icon: "pe-7s-signal", component: Gateway },
     { path: "/odr", name: "Open Device Registry", icon: "pe-7s-news-paper", component: Registry },
-    { path: "/transactions", name: "Transactions", icon: "pe-7s-gleam", component: Registry },
     { redirect: true, path:"/", to:"/client", name: "Client" }
 ];
 
