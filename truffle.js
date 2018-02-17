f0e78b02e20209e674781c7e42e575b8230a93a8
Carlos Beltran <imthatcarlos@gmail.com>
2018-02-17 13:24:07 -0600

    update packages

--- truffle.js
+++ truffle.js
@@ -1,3 +1,10 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = process.env.REACT_APP_INFURA_TOKEN || "e4X2v9uOjyfm58d51YBq";
+
+// metamask ropsten wallet
+var mnemonic = "output layer famous stuff unit swift clip crumble puppy involve tape hybrid";
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +12,10 @@ module.exports = {
       port: 8545,
       network_id: '*', // Match any network id,
       gas: 6721975 // 4712388
+    },
+    ropsten: {
+      network_id: 3,
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey)
     }
   }
 };
